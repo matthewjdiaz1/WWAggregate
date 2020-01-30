bdc2ed080c4b8fb0f8a0550b12e2f46543f92b96
Matthew-Jacob-Diaz <matthewjdiaz1@gmail.com>
2020-01-30 15:37:48 -0800

    styled ItemNotFoundModal

--- WWAggregate/components/ScanBarcode/ItemNotFoundModal.js
+++ WWAggregate/components/ScanBarcode/ItemNotFoundModal.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Modal } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Modal } from 'react-native';
 
 class ItemNotFoundModal extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      text: '',
+    };
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>ItemNotFoundModal</Text>
+        <Text style={styles.header}>Item not found</Text>
+        <Text style={styles.text}>Add a product name and scan a nutrition label to add it to your library</Text>
+        <Text style={styles.text}>Item Name</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="ex: Organic Applesauce"
+          onChangeText={(text) => this.setState({ text })}
+          value={this.state.text}>
+        </TextInput>
+        <View style={styles.buttonContainer}>
+          <Text style={styles.buttonCancel}>Cancel</Text>
+          <Text style={styles.buttonScan}>Scan Label</Text>
+        </View>
       </View>
     );
   }
@@ -22,13 +36,75 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    justifyContent: 'flex-end',
+    position: "relative",
+    // height: "100%",
+    paddingHorizontal: 20,
+    backgroundColor: "#E5E5E5",
+    color: '#000000',
+  },
+  header: {
+    textAlign: "center",
+    paddingTop: "30%",
+    fontSize: 36,
+    fontWeight: '500',
+    lineHeight: 46,
   },
   text: {
-    top: 25,
-    fontSize: 35,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: 'rgb(111, 218, 242)',
+    textAlign: "left",
+    paddingTop: "18%",
+    fontSize: 18,
+    fontWeight: '500',
+    lineHeight: 23,
   },
+  input: {
+    top: 10,
+    paddingHorizontal: 15,
+    paddingVertical: 15,
+    borderWidth: 1,
+    borderRadius: 4,
+    borderStyle: "solid",
+    borderColor: "#D2DBE2",
+    fontSize: 18,
+    fontWeight: '500',
+    lineHeight: 23,
+    color: "#B8B6B6",
+  },
+  buttonContainer: {
+    position: "absolute",
+    flexDirection: 'row',
+    justifyContent: "center",
+    alignItems: "center",
+    bottom: 0,
+    paddingBottom: "30%",
+    paddingHorizontal: 40,
+  },
+  buttonCancel: {
+    margin: 10,
+    paddingHorizontal: 29,
+    paddingVertical: 21,
+
+    // borderWidth: 1,
+    borderRadius: 4,
+    borderStyle: "solid",
+    borderColor: "white",
+
+    fontSize: 18,
+    fontWeight: '500',
+    lineHeight: 23,
+  },
+  buttonScan: {
+    margin: 10,
+    paddingHorizontal: 29,
+    paddingVertical: 21,
+    backgroundColor: "#6EDAF2",
+
+    // borderWidth: 1,
+    borderRadius: 4,
+    borderStyle: "solid",
+    borderColor: "white",
+
+    fontSize: 18,
+    fontWeight: '500',
+    lineHeight: 23,
+  }
 });
