4f70ea32308d48de73fb838411cd337399cf4dfd
Matthew-Jacob-Diaz <matthewjdiaz1@gmail.com>
2020-01-31 17:46:40 -0800

    added onPush

--- WWAggregate/components/Button/index.js
+++ WWAggregate/components/Button/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View, Text, TouchableHighlight } from 'react-native';
 
 import styles from './styles';
 
-const Button = ({ label, cta }) => (
-  <View style={cta ? styles.buttonViewCTA : styles.buttonView}>
-    <Text style={styles.button}>
-      {label}
-    </Text>
+const Button = ({ label, cta, onPress }) => (
+  <View style={cta ? styles.buttonViewCTA : styles.buttonView} onPress={onPress}>
+    <TouchableHighlight onPress={onPress}>
+      <Text style={styles.button}>
+        {label}
+      </Text>
+    </TouchableHighlight>
   </View>
 );
+
+Button.defaultProps = {
+  onPress: () => { },
+};
 
 export default Button;
