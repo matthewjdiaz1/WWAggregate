7ff7ee619376885259140e275e72df85acd8634d
Matthew-Jacob-Diaz <matthewjdiaz1@gmail.com>
2020-02-01 17:02:37 -0800

    added overlay and styled

--- WWAggregate/screens/ScanBarcodeScreen/index.js
+++ WWAggregate/screens/ScanBarcodeScreen/index.js
@@ -1,13 +1,36 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { Text, View } from 'react-native';
 import { withNavigation } from 'react-navigation';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 // import { Query } from 'react-apollo';
 // import { gql } from 'apollo-boost';
 
+import Svg, {
+  Circle,
+  Ellipse,
+  G,
+  TSpan,
+  TextPath,
+  Path,
+  Polygon,
+  Polyline,
+  Line,
+  Rect,
+  Use,
+  Image,
+  Symbol,
+  Defs,
+  LinearGradient,
+  RadialGradient,
+  Stop,
+  ClipPath,
+  Pattern,
+  Mask,
+} from 'react-native-svg';
+
 import styles from './styles';
 
-const ScanBarcodeScreen = (props) => {
+const ScanBarcodeScreen = ({ navigation }) => {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
 
@@ -18,15 +41,17 @@ const ScanBarcodeScreen = (props) => {
     })();
   }, []);
 
+  // TODO - apollo/postgres queries
   const handleBarCodeScanned = ({ type, data }) => {
     setScanned(true);
+
     // alert(`Bar code with type ${type} and data ${data} has been scanned!`);
 
     // check database to see if code has been scanned before
     // props.fetchBarcode(data);
     console.log('from scanner:', data);
-    setScanned(false);
-    props.navigation.navigate('ItemNotFound');
+    navigation.navigate('ItemNotFound');
+    // setScanned(false);
     //// if scanned before, show info
 
     // else, ask user to inter name of product
@@ -43,18 +68,35 @@ const ScanBarcodeScreen = (props) => {
   }
 
   return (
-    <View
-      style={{
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'flex-end',
-      }}>
-      <Text style={styles.text}>Scan a barcode</Text>
+    <View style={styles.container}>
       <BarCodeScanner
         onPress={() => props.toggle()}
         onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
         style={styles.scannerBorder}
       />
+      <Text style={styles.text}>Scan a barcode</Text>
+      <View style={styles.xButton}>
+        <Svg height="20" width="20" viewBox="0 0 300 300">
+          <Polygon
+            points="298.667,30.187 268.48,0 149.333,119.147 30.187,0 0,30.187 119.147,149.333 0,268.48 30.187,298.667     149.333,179.52 268.48,298.667 298.667,268.48 179.52,149.333"
+            fill="white"
+            strokeWidth="5"
+          />
+        </Svg>
+      </View>
+      <View style={styles.overlay}></View>
+      <Svg height="22" width="22" style={styles.cornerTL}>
+        <Polygon points="22,0 22,6 6,6 6,22 0,22 0,0" fill="white" />
+      </Svg>
+      <Svg height="22" width="22" style={styles.cornerTR}>
+        <Polygon points="0,0 22,0 22,22 16,22 16,6 0,6" fill="white" />
+      </Svg>
+      <Svg height="22" width="22" style={styles.cornerBR}>
+        <Polygon points="0,22 22,22 22,0 16,0 16,16 0,16" fill="white" />
+      </Svg>
+      <Svg height="22" width="22" style={styles.cornerBL}>
+        <Polygon points="0,0 0,22 22,22 22,16 6,16 6,0" fill="white" />
+      </Svg>
     </View>
   );
 }
